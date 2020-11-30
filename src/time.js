c8e8cc81091f4ee4997b368f050383bdb57b65e9
Hongbi <dyongdi@gmail.com>
2020-11-30 16:31:48 +0900

    feat: play버튼 클릭 시 10초 타이머 기능 구현

---
+++ src/time.js
@@ -0,0 +1,20 @@
+// play 버튼 클릭 시 10초 카운트 다운
+const timerBox = document.querySelector('.timer');
+let seconds = 9;
+play.addEventListener('click', timer);
+
+function timer() {
+  const time = setTimeout(() => {
+    if(seconds < 10) {
+      timerBox.innerHTML = `00:0${seconds}`;
+    } else {
+      timerBox.innerHTML = `00:${seconds}`;
+    }
+    seconds -= 1;
+    timer();
+  }, 1000);
+
+  if(seconds === -1) {
+    clearTimeout(time);
+  }
+}
