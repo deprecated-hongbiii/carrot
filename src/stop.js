17821735a6a73f32e7ba1115a09f50d36febb04b
Hongbi <dyongdi@gmail.com>
2020-12-01 22:14:16 +0900

    feat: 정지 버튼 클릭 시 팝업 메시지 추가

--- src/stop.js
+++ src/stop.js
@@ -1,12 +1,13 @@
 const stop = document.querySelector('.stop');
 const popUp = document.querySelector('.pop-up');
 const box = document.querySelector('.box');
+const popUpMessage = document.querySelector('.pop-up-message');
 
 stop.addEventListener('click', () => {
-  // clearTimeout();
   displayFlex(popUp);
   displayNone(stop);
   displayFlex(box);
+  popUpMessage.textContent = `Replay 👻`;
 })
 
 function displayNone(element) {
