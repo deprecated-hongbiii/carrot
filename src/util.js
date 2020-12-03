3c2aeb0c50c619bf7627494852c0b0209e3b7a35
Hongbi <dyongdi@gmail.com>
2020-12-03 20:09:32 +0900

    add: 좌표를 랜덤으로 결정하는 함수를 random.js에서 util.js로 옮김

--- src/util.js
+++ src/util.js
@@ -2,6 +2,17 @@ function $(selector) {
   return document.querySelector(selector)
 }
 
+const minAndMaxOfCoordinates = {
+  minX: 0,
+  maxX: 700,
+  minY: 0,
+  maxY: 150
+}
+
+function randomCoordinates(min, max) {
+  return Math.random() * (max - min) + min;
+};
+
 function displayNone(element) {
   addClassList(element, 'display-none');
 }
@@ -18,4 +29,4 @@ function removeClassList(element, className) {
   element.classList.remove(className);
 }
 
-export { $, displayNone, displayFlex, addClassList, removeClassList };
+export { $, minAndMaxOfCoordinates, randomCoordinates, displayNone, displayFlex, addClassList, removeClassList };
