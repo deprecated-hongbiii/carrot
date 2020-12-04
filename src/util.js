4be81f487218efcd8eb7df2d453026217d861db5
Hongbi <dyongdi@gmail.com>
2020-12-04 10:10:25 +0900

    add: dom 요소를 미리 찾아서 객체에 담아놓음

--- src/util.js
+++ src/util.js
@@ -2,6 +2,19 @@ function $(selector) {
   return document.querySelector(selector)
 }
 
+// dom 다 찾아놓기
+const $_ = {
+  play: $('.play'),
+  replay: $('.replay'),
+  stop: $('.stop'),
+  items: $('.items'),
+  popUp: $('.pop-up'),
+  popUpMessage: $('.pop-up-message'),
+  box: $('.box'),
+  timerBox: $('.timer'),
+  leftoverBox: $('.leftover')
+};
+
 const _ = {
   minX: 0,
   maxX: 700,
@@ -29,4 +42,4 @@ function removeClassList(element, className) {
   element.classList.remove(className);
 }
 
-export { $, _, randomCoordinates, displayNone, displayFlex, addClassList, removeClassList };
+export { $, $_, _, randomCoordinates, displayNone, displayFlex, addClassList, removeClassList };
